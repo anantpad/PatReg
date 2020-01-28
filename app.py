0ea3bcf927569a5cd4ec1d086a53c70b006cf287
anantpad <srid_24@hotmail.com>
2020-01-27 20:36:33 -0800

    First Commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.debug = True
 
 #config
-app.config["MONGO_URI"] = "mongodb://sridhar:asdf@cluster0-shard-00-00-aou9c.mongodb.net:27017,cluster0-shard-00-01-aou9c.mongodb.net:27017,cluster0-shard-00-02-aou9c.mongodb.net:27017/test_new?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb://sridhar:asdf@cluster0-shard-00-00-aou9c.mongodb.net:27017,cluster0-shard-00-01-aou9c.mongodb.net:27017,cluster0-shard-00-02-aou9c.mongodb.net:27017/pat_reg?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 # app.config["MONGO_URI"] = "mongodb://sridhar:asdf@cluster0-aou9c.mongodb.net/test?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 print(mongo.db)
@@ -30,12 +30,26 @@ def login():
 @app.route("/patientinfo", methods = ['GET', 'POST'])
 def patientinfo():
     if request.method == 'GET':
-        name = "Sridhar"
-        return render_template("patient_reg.html", firstname = name)
+        # name = "Sridhar"
+        return render_template("patient_reg.html") #, firstname = name)
     elif request.method == "POST":
         firstname = request.form["firstname"]
-        mongo.db.patient_info.insert_one({"firstname":firstname})
-        return firstname
+        lastname = request.form["lastname"]
+        mi = request.form["mi"]
+        patientid = request.form["patientid"]
+        birthdate = request.form["birthdate"]
+        gender = request.form["gender"]
+        race = request.form["race"]
+        address = request.form["address"]
+        city = request.form["city"]
+        state = request.form["state"]
+        country = request.form["country"]
+        zip = request.form["zip"]
+        loginname = request.form["loginname"]
+        password = request.form["password"]
+        mongo.db.patient_info.insert_one(
+            {"firstname":firstname, "lastname":lastname, "mi":mi, "patientid":patientid, "birthdate":birthdate, "gender":gender, "race":race, "address":address, "city":city, "state":state, "country":country,"zip":zip, "loginname":loginname, "password":password})
+        return redirect("/patientinfo")
 
 #run
 if __name__ == "__main__":
